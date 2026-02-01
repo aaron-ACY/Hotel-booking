84f37439df44760d5aa4e67b466ec5cf7cd84224
aaron--acy <huudanle123@gmail.com>
2026-02-01 19:41:17 +0700

    Change

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,3 +1,5 @@
 <h1>Home page view</h1>
 
-<img src="<?=ROOT?>/assets/img/images.jpg">
+<!-- <img src="<?=ROOT?>/assets/img/images.jpg" style="height=300px"> -->
+ <img src="<?=ROOT?>/assets/img/images.jpg" style="height:300px; object-fit:cover;">
+
